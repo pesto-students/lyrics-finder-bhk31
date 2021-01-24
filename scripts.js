7af3492201ac2fe9fa28332f55f12af14b488a2b
bhk31 <bhkhandelwal92@gmail.com>
2021-01-24 23:03:05 +0530

    add script and css file

---
+++ scripts.js
@@ -0,0 +1,70 @@
+// Example POST method implementation:
+async function postData(url = '',) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+let getLyricsLink = (buttonName) => document.getElementsByClassName(buttonName);
+
+let addEventToButton = (buttonName, event, fn) => {
+    Array.from(buttonName).forEach(function (element) {
+        element.addEventListener(event, fn);
+    });
+}
+
+let modal = document.getElementById("myModal");
+
+  document.getElementById('search').onclick = function() {
+    let song = getInputElementFromForm('lyrics-form', 'song');
+
+    let searchResult = postData('https://api.lyrics.ovh/suggest/' + song);
+    searchResult.then(data => {
+    // console.log(data.data, data.total); // JSON data parsed by `data.json()` call
+    let itemElement = '';
+    for(el of data.data) {
+      itemElement += `<div class="item" >
+            <img src="${el.album.cover}" alt="">
+            <div style="display: flex; flex-direction: column; margin: 1%;">
+              <p class="title"><b>Title:</b><span>${el.album.title}</p>
+              <p class="artist"><b>Artist:</b>${el.artist.name}</p>
+              <div display:flex;>
+              <a href="${el.preview}" target="_blank">Listen</a>
+              <a href="#" class="view-lyrics" data-artist="${el.artist.name}" data-title="${el.album.title}" onclick="getLyrics('${el.artist.name}','${el.album.title}')">View Lyrics</a>
+            </div>  
+            </div>
+            
+            
+            <img src="${el.artist.picture}" alt="">
+          </div>`
+    }
+
+    getSelectedElement('list-container').innerHTML = itemElement;
+  });
+    
+  }
+
+const getLyrics = (artist, title) => {
+  
+}
+
+  function getInputElementFromForm(form, input) {
+  let formData = new FormData(getSelectedElement(form));
+  return formData.get(input);
+  }
+  
+  function getSelectedElement(element) {
+  return document.getElementById(element);
+}
