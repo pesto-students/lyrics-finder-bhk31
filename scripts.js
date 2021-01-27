f96c1507104df031206d217e5d40708cd56e4c14
bhk31 <bhkhandelwal92@gmail.com>
2021-01-28 02:09:46 +0530

    add code for home page

--- scripts.js
+++ scripts.js
@@ -17,54 +17,62 @@ async function postData(url = '',) {
 }
 
 
-let getLyricsLink = (buttonName) => document.getElementsByClassName(buttonName);
+// let modal = document.getElementById("myModal");
 
-let addEventToButton = (buttonName, event, fn) => {
-    Array.from(buttonName).forEach(function (element) {
-        element.addEventListener(event, fn);
-    });
+document.getElementById('search').onclick = function () {
+  let song = getInputElementFromForm('lyrics-form', 'song');
+  getSuggestions(song);
 }
 
-let modal = document.getElementById("myModal");
+// const getLyrics = (artist, title) => {
+//   console.log(artist);
+//   console.log(title);
 
-  document.getElementById('search').onclick = function() {
-    let song = getInputElementFromForm('lyrics-form', 'song');
+//   let url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+//   let lyricsPromise = postData(url)
+//   lyricsPromise.then(data => {
+//     let modal = document.getElementById("myModal");
+//     let content = document.getElementById("content");
+//     modal.style.display = "flex";
+//     content.innerText = data.lyrics;
+//   });
+// }
 
-    let searchResult = postData('https://api.lyrics.ovh/suggest/' + song);
-    searchResult.then(data => {
+function getInputElementFromForm(form, input) {
+  let formData = new FormData(getSelectedElement(form));
+  return formData.get(input);
+}
+
+function getSelectedElement(element) {
+  return document.getElementById(element);
+}
+
+function getSuggestions(song) {
+  let searchResult = postData('https://api.lyrics.ovh/suggest/' + song);
+  searchResult.then(data => {
     // console.log(data.data, data.total); // JSON data parsed by `data.json()` call
     let itemElement = '';
-    for(el of data.data) {
+    for (el of data.data) {
       itemElement += `<div class="item" >
-            <img src="${el.album.cover}" alt="">
-            <div style="display: flex; flex-direction: column; margin: 1%;">
-              <p class="title"><b>Title:</b><span>${el.album.title}</p>
-              <p class="artist"><b>Artist:</b>${el.artist.name}</p>
-              <div display:flex;>
-              <a href="${el.preview}" target="_blank">Listen</a>
-              <a href="#" class="view-lyrics" data-artist="${el.artist.name}" data-title="${el.album.title}" onclick="getLyrics('${el.artist.name}','${el.album.title}')">View Lyrics</a>
-            </div>  
-            </div>
-            
-            
-            <img src="${el.artist.picture}" alt="">
-          </div>`
+          <img src="${el.album.cover}" alt="">
+          <div style="display: flex; flex-direction: column; margin: 1%;">
+            <p class="title"><b>Title:</b><span>${el.album.title}</p>
+            <p class="artist"><b>Artist:</b>${el.artist.name}</p>
+            <div display:flex;>
+            <a href="${el.preview}" target="_blank">Listen</a>
+            <a href="#" class="view-lyrics" data-artist="${el.artist.name}" data-title="${el.album.title}" onclick="getLyrics('${el.artist.name}','${el.album.title}')">View Lyrics</a>
+          </div>  
+          </div>
+          
+          
+          <img src="${el.artist.picture}" alt="">
+        </div>`
     }
 
     getSelectedElement('list-container').innerHTML = itemElement;
   });
-    
-  }
-
-const getLyrics = (artist, title) => {
-  
 }
 
-  function getInputElementFromForm(form, input) {
-  let formData = new FormData(getSelectedElement(form));
-  return formData.get(input);
-  }
-  
-  function getSelectedElement(element) {
-  return document.getElementById(element);
+function searchArtist(artist) {
+  getSuggestions(artist);
 }
